11677a16139e01f3240c7257fa2bbf6808bfde3a
Gregor Santner <gsantner@mailbox.org>
2019-09-02 01:09:16 +0200

    Reindent

--- geanygsantnerutils.c
+++ geanygsantnerutils.c
@@ -1,8 +1,14 @@
+//######################################################################################################
+// vim: sw=4 ts=4 ft=sh noexpandtab:
+// while [ 1 -eq 1 ] ; do make install && geany -v && sleep 0.1; done
+// msgwin_status_add ("[%s] AFTER C", label);
+//######################################################################################################
+
+
+// Includes
 #include "geanyplugin.h"
 #include <stdio.h>
 
-// while [ 1 -eq 1 ] ; do make install && geany -v && sleep 0.1; done
-// msgwin_status_add ("[%s] AFTER C", label);
 
 // Plugin setup
 #define	PLUGIN_NAME	"geanygsantnerutils"
@@ -13,19 +19,20 @@ PLUGIN_SET_INFO("gsantner utils", "Description", "1.0", "Gregor Santner <gsantne
 
 enum {
 	KEYBINDING_JSON_REFORMAT,
-	NUM_KEYS
+	NUM_KEYS,
+	KEYBINDING_SHOW_FAVOURITES,
 };
-
 static struct {
-  // json_reformat
+	// json_reformat
 	GtkWidget	*menuitem_json_reformat;
 
-  // MenuItems with file references
+	// MenuItems with file references
 	GList   	menuitem_list_open_file_in_callback_arg;
 
-  // Favourites
-	GtkWidget	*submenu_favourites;
+	// Favourites
 	GtkWidget	*menuitem_favourites;
+	GtkWidget	*toolbar_item_favourites;
+	GtkWidget	*submenu_favourites;
 
 
 	GeanyKeyGroup	*key_group;
@@ -35,17 +42,17 @@ static struct {
 
 // Frees input string
 static gchar* g_strreplace(gchar *text, const gchar *search, const gchar *replace, gboolean free_input) {
-  char **split = g_strsplit(text, search, -1);
-  if (free_input) {
-    g_free(text);
-  }
-  text = g_strjoinv(replace, split);
-  g_strfreev(split);
-  return text;
+	char **split = g_strsplit(text, search, -1);
+	if (free_input) {
+		g_free(text);
+	}
+	text = g_strjoinv(replace, split);
+	g_strfreev(split);
+	return text;
 }
 
 static gchar *geany_config_filepath() {
-  return g_build_filename(geany_data->app->configdir, "geany.conf", NULL);
+	return g_build_filename(geany_data->app->configdir, "geany.conf", NULL);
 }
 
 // Get settings object for standard geany.conf file
@@ -101,25 +108,25 @@ static void exec_json_reformat() {
 	unlink(tmp_infile);
 	unlink(tmp_outfile);
 
-  // Evaluate result
-  if (exitc == 0) {
-    // Set reformatted text to UI
-    sci_start_undo_action(sci);
-    sci_set_text(sci, text);
-    sci_end_undo_action(sci);
-  } else if (exitc > 256) {
-    msgwin_switch_tab(MSG_MESSAGE, 1);
-    msgwin_msg_add(COLOR_RED, -1, doc, "[%s] json_reformat not installed. Contained in package yajl-tools", filename);
-  } else {
-    msgwin_switch_tab(MSG_MESSAGE, 1);
-    msgwin_msg_add(COLOR_RED, -1, doc, "[%s] json_reformat error, code %d. The content seems not to be valid JSON.", filename, exitc);
-  }
+	// Evaluate result
+	if (exitc == 0) {
+		// Set reformatted text to UI
+		sci_start_undo_action(sci);
+		sci_set_text(sci, text);
+		sci_end_undo_action(sci);
+	} else if (exitc > 256) {
+		msgwin_switch_tab(MSG_MESSAGE, 1);
+		msgwin_msg_add(COLOR_RED, -1, doc, "[%s] json_reformat not installed. Contained in package yajl-tools", filename);
+	} else {
+		msgwin_switch_tab(MSG_MESSAGE, 1);
+		msgwin_msg_add(COLOR_RED, -1, doc, "[%s] json_reformat error, code %d. The content seems not to be valid JSON.", filename, exitc);
+	}
 
-  // Free resources
-  free(text);
-  free(syscmd);
-  free(tmp_outfile);
-  free(filename);
+	// Free resources
+	free(text);
+	free(syscmd);
+	free(tmp_outfile);
+	free(filename);
 }
 
 //######################################################################################################
@@ -133,6 +140,9 @@ static gboolean item_activated_by_keybinding_id(guint keyid) {
 	case KEYBINDING_JSON_REFORMAT:
 		exec_json_reformat();
 		break;
+	case KEYBINDING_SHOW_FAVOURITES:
+		gtk_menu_popup_at_pointer(GTK_MENU(gtk_menu_tool_button_get_menu(plugin_private.toolbar_item_favourites)), NULL);
+		break;
 	}
 	return 0;
 }
@@ -143,152 +153,168 @@ static void item_activated_by_id(GtkWidget *wid, gpointer eventdata) {
 
 // Restyle the sidebar (containing "symbols" "files" "projects" etc)
 static void restyle_sidebar() {
-  GtkCssProvider *    cssProvider     = gtk_css_provider_new();
-  gtk_css_provider_load_from_data(cssProvider, ""
-    "*                       { background-color: #3A3D3F; } "
-    ".myNotebook tab         { background-color: #3A3D3F; border-right-style: solid; border-color: #808000; border-bottom-width: 1px; border-right-width: 2px; border-bottom-right-radius: 7px; border-top-right-radius: 7px; border-left-width: 0px; }"
-    ".myNotebook tab:checked { background-color: #AD8641; }"
-    "*                       {  }"
-    , -1, NULL);
+	GtkCssProvider *    cssProvider     = gtk_css_provider_new();
+	gtk_css_provider_load_from_data(cssProvider, ""
+		"*                       { background-color: #3A3D3F; } "
+		".myNotebook tab         { background-color: #3A3D3F; border-right-style: solid; border-color: #808000; border-bottom-width: 1px; border-right-width: 2px; border-bottom-right-radius: 7px; border-top-right-radius: 7px; border-left-width: 0px; }"
+		".myNotebook tab:checked { background-color: #AD8641; }"
+		"*                       {  }"
+		, -1, NULL);
 
-  GtkNotebook *sidebarNotebook = GTK_NOTEBOOK(ui_lookup_widget(geany_data->main_widgets->window, "notebook3"));
-  GtkStyleContext * sidebarNotebookStyleContext = gtk_widget_get_style_context(GTK_WIDGET(sidebarNotebook));
-  gtk_style_context_add_class(sidebarNotebookStyleContext, "myNotebook");
-  gtk_style_context_add_provider(gtk_widget_get_style_context(GTK_WIDGET(sidebarNotebook)), GTK_STYLE_PROVIDER(cssProvider), GTK_STYLE_PROVIDER_PRIORITY_USER);
-  for (GList *iterator = gtk_container_get_children(GTK_CONTAINER(sidebarNotebook)); iterator; iterator = iterator->next) {
-      GtkLabel *sidebarLabel = GTK_LABEL(gtk_notebook_get_tab_label(sidebarNotebook, iterator->data));
-      gtk_label_set_angle(sidebarLabel, 90);
+	GtkNotebook *sidebarNotebook = GTK_NOTEBOOK(ui_lookup_widget(geany_data->main_widgets->window, "notebook3"));
+	GtkStyleContext * sidebarNotebookStyleContext = gtk_widget_get_style_context(GTK_WIDGET(sidebarNotebook));
+	gtk_style_context_add_class(sidebarNotebookStyleContext, "myNotebook");
+	gtk_style_context_add_provider(gtk_widget_get_style_context(GTK_WIDGET(sidebarNotebook)), GTK_STYLE_PROVIDER(cssProvider), GTK_STYLE_PROVIDER_PRIORITY_USER);
+	for (GList *iterator = gtk_container_get_children(GTK_CONTAINER(sidebarNotebook)); iterator; iterator = iterator->next) {
+		GtkLabel *sidebarLabel = GTK_LABEL(gtk_notebook_get_tab_label(sidebarNotebook, iterator->data));
+		gtk_label_set_angle(sidebarLabel, 90);
 
-      GString *text = g_string_new(gtk_label_get_text(sidebarLabel));
-      g_string_prepend(text, " <b><span font_desc='Monospace 10'>🔻");
-      g_string_append(text, "</span></b>  ");
-      gtk_label_set_markup(sidebarLabel, g_string_free(text, FALSE));
-  }
+		GString *text = g_string_new(gtk_label_get_text(sidebarLabel));
+		g_string_prepend(text, " <b><span font_desc='Monospace 10'>🔻");
+		g_string_append(text, "</span></b>  ");
+		gtk_label_set_markup(sidebarLabel, g_string_free(text, FALSE));
+	}
 }
 
 // Hide some clutter options from menus
 static void hide_clutter() {
-  GtkToolbar *toolbar = GTK_TOOLBAR(geany_data->main_widgets->toolbar);
+	GtkToolbar *toolbar = GTK_TOOLBAR(geany_data->main_widgets->toolbar);
 
-  gtk_widget_hide(ui_lookup_widget(geany_data->main_widgets->window, "menu_open_selected_file1"));
-  gtk_widget_hide(ui_lookup_widget(geany_data->main_widgets->window, "menu_reload_as1"));
-  gtk_widget_hide(ui_lookup_widget(geany_data->main_widgets->window, "menu_close_all1"));
-  gtk_widget_hide(ui_lookup_widget(geany_data->main_widgets->window, "close_other_documents1"));
-  gtk_widget_hide(ui_lookup_widget(geany_data->main_widgets->window, "menu_reload_as1"));
+	gtk_widget_hide(ui_lookup_widget(geany_data->main_widgets->window, "menu_open_selected_file1"));
+	gtk_widget_hide(ui_lookup_widget(geany_data->main_widgets->window, "menu_reload_as1"));
+	gtk_widget_hide(ui_lookup_widget(geany_data->main_widgets->window, "menu_close_all1"));
+	gtk_widget_hide(ui_lookup_widget(geany_data->main_widgets->window, "close_other_documents1"));
+	gtk_widget_hide(ui_lookup_widget(geany_data->main_widgets->window, "menu_reload_as1"));
 
-  for (int i=0; i <  gtk_toolbar_get_n_items(toolbar); i++) {
-    GtkToolItem *item = gtk_toolbar_get_nth_item(toolbar, i);
-    switch (i) {
-      case -1:
-        gtk_widget_hide(GTK_WIDGET(item));
-        break;
-    }
-  }
+	/* // Can be configured by settings with drag'n'drop
+	for (int i=0; i <  gtk_toolbar_get_n_items(toolbar); i++) {
+		GtkToolItem *item = gtk_toolbar_get_nth_item(toolbar, i);
+		switch (i) {
+		case 2: // save
+		case 3: // save all
+		case 5: // reload
+		case 6: // close
+		case 8: // one step forward in editor (ctrl-left)
+		case 9: // one step forward in editor (ctrl-right)
+		case 21: // goto line button (# input in textfield + enter does the same)
+			gtk_widget_hide(GTK_WIDGET(item));
+			break;
+		}
+	}*/
 }
 
 static void add_favourites_to_menu(const gchar *dir_home, GKeyFile* config, const GtkMenu *file_menu) {
-  gchar *str;
-  gchar **strarr;
+	gchar *str;
+	gchar **strarr;
 
-  // Favourites
-  str = utils_get_setting_string(config, PLUGIN_NAME, "favourites", "");
-  if (g_utf8_strlen(str, -1) > 0) {
-    strarr = g_strsplit(str, ";", -1);
+	// Favourites
+	str = utils_get_setting_string(config, PLUGIN_NAME, "favourites", "");
+	if (g_utf8_strlen(str, -1) > 0) {
+		strarr = g_strsplit(str, ";", -1);
 
-    // Setup submenu
-    plugin_private.submenu_favourites = gtk_menu_new();
-    plugin_private.menuitem_favourites = ui_image_menu_item_new("gtk-about", "Favouriten");
-    gtk_menu_item_set_submenu(GTK_MENU_ITEM(plugin_private.menuitem_favourites), plugin_private.submenu_favourites);
- 
-    // Fill submenu
-    for (int i=0; (strarr[i] != NULL && strarr[i+1] != NULL); i+=2) {
-      GtkWidget* menuitem = NULL;
-      gchar *label = g_strreplace(strarr[i], "_", "__", 0); // First underscore is for keybinding and doesn't show up
+		// Setup submenu
+		plugin_private.submenu_favourites = gtk_menu_new();
+		for (int i=0; (strarr[i] != NULL && strarr[i+1] != NULL); i+=2) {
+			GtkWidget* menuitem = NULL;
+			gchar *label = g_strreplace(strarr[i], "_", "__", 0); // First underscore is for keybinding and doesn't show up
 
-      // Create submenu item
-      if (g_str_equal(label, "---")) { /* Separator */ 
-        menuitem = gtk_separator_menu_item_new(); i--;
-      } else { /* Filepath */ 
-        gchar *filepath = g_strreplace(strarr[i+1], "$HOME", dir_home, 0);
-        if (g_file_test(filepath, G_FILE_TEST_IS_REGULAR)) {
-          menuitem = ui_image_menu_item_new(NULL, _(label));
-          g_signal_connect(G_OBJECT(menuitem), "activate", G_CALLBACK(item_activated_open_file_in_callback_arg), filepath);
-        }
-      }
+			// Create submenu item
+			if (g_str_equal(label, "---")) { /* Separator */ 
+				menuitem = gtk_separator_menu_item_new(); i--;
+			} else { /* Filepath */ 
+				gchar *filepath = g_strreplace(strarr[i+1], "$HOME", dir_home, 0);
+				if (g_file_test(filepath, G_FILE_TEST_IS_REGULAR)) {
+					menuitem = ui_image_menu_item_new(NULL, _(label));
+					g_signal_connect(G_OBJECT(menuitem), "activate", G_CALLBACK(item_activated_open_file_in_callback_arg), filepath);
+				}
+			}
 
-      // Add item to submenu
-      if (menuitem != NULL) {
-        gtk_menu_shell_append(GTK_MENU_SHELL(plugin_private.submenu_favourites), menuitem);
-        gtk_widget_show_all(menuitem);
-#pragma GCC diagnostic ignored "-Wunused-result"
-        g_list_append(&plugin_private.menuitem_list_open_file_in_callback_arg, menuitem);
-      }
-    }
+			// Add item to submenu
+			if (menuitem != NULL) {
+				gtk_menu_shell_append(GTK_MENU_SHELL(plugin_private.submenu_favourites), menuitem);
+				gtk_widget_show_all(menuitem);
+				#pragma GCC diagnostic ignored "-Wunused-result"
+				g_list_append(&plugin_private.menuitem_list_open_file_in_callback_arg, menuitem);
+			}
+		}
 
-    // Show submenu
-    gtk_widget_show_all(plugin_private.submenu_favourites);
-    gtk_widget_show_all(plugin_private.menuitem_favourites);
-    gtk_menu_shell_insert(GTK_MENU_SHELL(file_menu), plugin_private.menuitem_favourites, 4);
-  }
-  free(str);
+		// Show submenu - at menu
+		plugin_private.menuitem_favourites = ui_image_menu_item_new("gtk-about", "Favouriten");
+		gtk_menu_item_set_submenu(GTK_MENU_ITEM(plugin_private.menuitem_favourites), plugin_private.submenu_favourites);
+		gtk_widget_show_all(plugin_private.submenu_favourites);
+		gtk_widget_show_all(plugin_private.menuitem_favourites);
+		gtk_menu_shell_insert(GTK_MENU_SHELL(file_menu), plugin_private.menuitem_favourites, 4);
+
+		// Show submenu - at toolbar
+		plugin_private.toolbar_item_favourites = gtk_menu_tool_button_new(NULL, NULL);
+		gtk_tool_button_set_icon_name(plugin_private.toolbar_item_favourites, "gtk-about");
+		gtk_menu_tool_button_set_menu(plugin_private.toolbar_item_favourites, plugin_private.submenu_favourites);
+		gtk_toolbar_insert(GTK_TOOLBAR(geany_data->main_widgets->toolbar), plugin_private.toolbar_item_favourites, 1);
+		gtk_widget_show_all(plugin_private.toolbar_item_favourites);
+		g_signal_connect(G_OBJECT(plugin_private.toolbar_item_favourites), "clicked", G_CALLBACK(item_activated_by_id), GPOINTER_TO_INT(KEYBINDING_SHOW_FAVOURITES));
+	}
+	free(str);
 }
+
+
 
 //######################################################################################################
 
 // Init plugin
 void plugin_init(GeanyData *geany_data) {
-  GKeyFile *config   = geany_config();
-  const GtkMenu *file_menu = GTK_MENU(ui_lookup_widget(geany_data->main_widgets->window, "file1_menu"));
-  const gchar *dir_home    = g_get_home_dir();
+	GKeyFile *config   = geany_config();
+	const GtkMenu *file_menu = GTK_MENU(ui_lookup_widget(geany_data->main_widgets->window, "file1_menu"));
+	const gchar *dir_home    = g_get_home_dir();
 
-  // Setup Keybindings
+	// Hide some clutter options from menus
+	hide_clutter();
+
+	// Setup Keybindings
 	plugin_private.key_group = plugin_set_key_group(geany_plugin, PLUGIN_NAME, NUM_KEYS, item_activated_by_keybinding_id);
 
-  // JSON Reformat
+	// JSON Reformat
 	plugin_private.menuitem_json_reformat = ui_image_menu_item_new(NULL, _("json__reformat"));
 	g_signal_connect(G_OBJECT(plugin_private.menuitem_json_reformat), "activate", G_CALLBACK(item_activated_by_id), GINT_TO_POINTER(TRUE));
 	gtk_widget_show_all(plugin_private.menuitem_json_reformat);
 	gtk_container_add(GTK_CONTAINER(geany_data->main_widgets->tools_menu), plugin_private.menuitem_json_reformat);
 	keybindings_set_item(plugin_private.key_group, KEYBINDING_JSON_REFORMAT, NULL, 0, 0, "json_reformat", _("json_reformat"), plugin_private.menuitem_json_reformat);
 
-  // Restyle sidebar
-  restyle_sidebar();
+	// Restyle sidebar
+	restyle_sidebar();
 
-  // Add favourites to the file menu
-  add_favourites_to_menu(dir_home, config, file_menu);
+	// Add favourites to the file menu
+	add_favourites_to_menu(dir_home, config, file_menu);
 
-  // Hide some clutter options from menus
-  hide_clutter();
 
 
 /////
-  // Testing code
-  //gtk_label_set_angle(ui_lookup_widget(geany_data->main_widgets->window, "label135"), 90);
-  //gtk_label_set_angle(ui_lookup_widget(geany_data->main_widgets->window, "label136"), 90);
-  //gtk_label_set_angle(ui_lookup_widget(geany_data->main_widgets->window, "label137"), 90);
+	// Testing code
+	//gtk_label_set_angle(ui_lookup_widget(geany_data->main_widgets->window, "label135"), 90);
+	//gtk_label_set_angle(ui_lookup_widget(geany_data->main_widgets->window, "label136"), 90);
+	//gtk_label_set_angle(ui_lookup_widget(geany_data->main_widgets->window, "label137"), 90);
 
-  //utils_get_setting_string(config, PLUGIN_NAME, "color_scheme", "aaa-solarized-greg.conf");
-  //g_key_file_set_string(config, "geany", "color_scheme", "a");
-  //g_key_file_save_to_file(geany_config,geany_config_filepath(), NULL);
-  //filetypes_reload();
+	//utils_get_setting_string(config, PLUGIN_NAME, "color_scheme", "aaa-solarized-greg.conf");
+	//g_key_file_set_string(config, "geany", "color_scheme", "a");
+	//g_key_file_save_to_file(geany_config,geany_config_filepath(), NULL);
+	//filetypes_reload();
 //////
 
-  // Free resources
-  free((char*) dir_home);
+	// Free resources
+	free((char*) dir_home);
 }
 
 
 // Plugin destructor
 // gtk_widget_destroy includes free()
 void plugin_cleanup(void) {
-  GList *iterator = NULL;
+	GList *iterator = NULL;
 	gtk_widget_destroy(plugin_private.menuitem_json_reformat);
 	gtk_widget_destroy(plugin_private.submenu_favourites);
 	gtk_widget_destroy(plugin_private.menuitem_favourites);
 
- for (iterator = &(plugin_private.menuitem_list_open_file_in_callback_arg); iterator; iterator = iterator->next) {
-  if (GTK_IS_WIDGET(iterator->data)) {
-    gtk_widget_destroy(iterator->data);
-  }
- }
+	for (iterator = &(plugin_private.menuitem_list_open_file_in_callback_arg); iterator; iterator = iterator->next) {
+		if (GTK_IS_WIDGET(iterator->data)) {
+			gtk_widget_destroy(iterator->data);
+		}
+	}
 }
